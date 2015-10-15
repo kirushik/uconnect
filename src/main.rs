c9fbc11a764102200d26db6dd43473208e8de781
Kirill Pimenov <kpimenov@suse.de>
2015-10-15 12:00:21 +0200

    Parses CLI arguments with `clap`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+  let _matches = App::new("uconnect")
+                    .version("0.1.0")
+                    .author("Kirill Pimenov <kpimenov@suse.de>")
+                    .about("Rust reimplementation of the SUSE Connect tool.")
+                    .get_matches();
+  println!("Hello, world!");
 }
