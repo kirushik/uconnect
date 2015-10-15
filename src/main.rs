9d48bad1c9856514cf52e9ab076369d2668ebbed
Kirill Pimenov <kpimenov@suse.de>
2015-10-15 12:04:34 +0200

    Debug CLI argument (noop)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ fn main() {
                     .version("0.1.0")
                     .author("Kirill Pimenov <kpimenov@suse.de>")
                     .about("Rust reimplementation of the SUSE Connect tool.")
+                    .arg(Arg::with_name("debug")
+                             .short("d")
+                             .long("debug")
+                             .help("Enable debugging output"))
                     .get_matches();
   println!("Hello, world!");
 }
