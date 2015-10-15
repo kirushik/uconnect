fc432a433b46d4f570e44d90d33322c5a7389844
Kirill Pimenov <kpimenov@suse.de>
2015-10-15 12:09:07 +0200

    Debugging output actually works

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+// Options parsing
 extern crate clap;
 use clap::{Arg, App};
 
+// Logging
+#[macro_use]
+extern crate log;
+extern crate flexi_logger;
+
 fn main() {
-  let _matches = App::new("uconnect")
+  let matches = App::new("uconnect")
                     .version("0.1.0")
                     .author("Kirill Pimenov <kpimenov@suse.de>")
                     .about("Rust reimplementation of the SUSE Connect tool.")
@@ -11,5 +17,15 @@ fn main() {
                              .long("debug")
                              .help("Enable debugging output"))
                     .get_matches();
+
+  if matches.is_present("debug") {
+    enable_debug();
+  };
+
+  debug!("It works!");
   println!("Hello, world!");
+}
+
+fn enable_debug() {
+  flexi_logger::init(flexi_logger::LogConfig::new(), Some("uconnect=debug".to_string())).unwrap();
 }
